3104fb3e2c0eae34bc6fce1f01a74b5b3488d8d2
Binit Kumar Lal <a91843@Mac.lan>
2026-05-28 00:42:59 +0530

    java self study & practice

---
+++ conditional.java
@@ -0,0 +1,65 @@
+
+//import java.util.*;
+
+
+//         Scanner sc = new Scanner(System.in);
+//         int age = sc.nextInt();
+//         if (age >= 18) {
+//             System.out.println("Adult");
+//         } else {
+//             System.out.println("Not Adult");
+//         }
+//         sc.close();
+//     }
+// }
+
+
+// public class conditional {
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter a number: ");
+//         int num = sc.nextInt();
+
+//         if(num % 2 == 0)
+//             System.out.println(num + " is even");
+//         else
+//             System.out.println(num + " is odd");
+//     }
+// }
+
+// public class conditional{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+       
+//         if(a==b){
+//             System.out.println("equal");
+//         } else if(a > b){
+//             System.out.println("a is greater");
+//         } else {
+//             System.out.println("a is smaller");
+//         }
+            
+//     }
+// }
+
+// SWITCH
+// public class conditional{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int button = sc.nextInt();
+//         switch(button){
+//             case 1 : System.out.println("Hello");
+//             break;
+//             case 2 : System.out.println("Namaste");
+//             break;
+//             case 3 : System.out.println("Bonjour");
+//             break;
+//             default: System.out.println("Invalid");
+//         }
+//     }
+// }
